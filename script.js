9aa08a44004fdccffb2e80ae5e1296f8db3ba5df
maristelasantos-boop <maristela.santos@sempreceub.com>
2026-09-16 17:12:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+# Query: 
+# ContextLines: 1
+
+// ==========================================
+// SAÚDE RENOVADA
+// JAVASCRIPT DA LANDING PAGE
+// ==========================================
+
+
+// Seleciona o formulário
+const form = document.getElementById("contactForm");
+
+
+// Seleciona a mensagem
+const formMessage = document.getElementById("formMessage");
+
+
+// Evento de envio do formulário
+form.addEventListener("submit", function (event) {
+
+    // Impede o envio real do formulário
+    event.preventDefault();
+
+    // Exibe mensagem para o usuário
+    formMessage.textContent =
+        "Formulário preenchido com sucesso! Esta é uma demonstração acadêmica.";
+
+    // Limpa os campos
+    form.reset();
+
+});
